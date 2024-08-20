a86864963897ed1079a4b84cd313edc62c960b34
JasmineSizhen <sizhen.li@sanofi.com>
2024-08-20 19:27:55 +0000

    run script

---
+++ submit.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --mem=32g
+#SBATCH --gres=gpu:rtx2080:1
+#SBATCH -c 3
+#SBATCH --output=example_4.out
+
+source activate mlfold
+
+folder_with_pdbs="1n8z"
+
+output_dir="outputs/1n8z_1000k"
+if [ ! -d $output_dir ]
+then
+    mkdir -p $output_dir
+fi
+
+
+path_for_parsed_chains=$output_dir"/parsed_pdbs.jsonl"
+path_for_assigned_chains=$output_dir"/assigned_pdbs.jsonl"
+path_for_fixed_positions=$output_dir"/fixed_pdbs.jsonl"
+chains_to_design="H"
+#The first amino acid in the chain corresponds to 1 and not PDB residues index for now.
+fixed_positions='1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 101 102 103 104 105 106 117 118 119 120 121 122 123 124 125 126 127 128'
+
+python helper_scripts/parse_multiple_chains.py --input_path=$folder_with_pdbs --output_path=$path_for_parsed_chains
+python helper_scripts/assign_fixed_chains.py --input_path=$path_for_parsed_chains --output_path=$path_for_assigned_chains --chain_list "$chains_to_design"
+python helper_scripts/make_fixed_positions_dict.py --input_path=$path_for_parsed_chains --output_path=$path_for_fixed_positions --chain_list "$chains_to_design" --position_list "$fixed_positions"
+
+python -u protein_mpnn_run.py \
+        --path_to_model_weights "abmpnn_model_weights" \
+        --model_name "abmpnn" \
+        --jsonl_path $path_for_parsed_chains \
+        --chain_id_jsonl $path_for_assigned_chains \
+        --fixed_positions_jsonl $path_for_fixed_positions \
+        --out_folder $output_dir \
+        --num_seq_per_target 1000000 \
+        --sampling_temp "0.3" \
+        --seed 37 \
+        --batch_size 16
+
+# --path_to_model_weights "abmpnn_model_weights" \
+# --model_name "abmpnn" \
